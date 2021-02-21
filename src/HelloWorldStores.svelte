ac4daef28b3831eb8d253a381be7136d328d774b
agoldstein03 <38170312+agoldstein03@users.noreply.github.com>
2021-02-20 22:19:10 -0500

    Replace Snowpack with Rollup
    :frowning:
    But it fixes the UI library styles issues

--- src/HelloWorldStores.svelte
+++ src/HelloWorldStores.svelte
@@ -1,20 +1,14 @@
 <template lang="pug">
-	MaterialApp(theme="dark")
-		h1 Hello {name}!
-		p With stores this time.
-		HelloWorldStoresDisplay
-		button(on:click!="{() => {$count++}}") Click to go from {$count} to {countPlusOne}
-		div.d-flex.justify-space-around
-			Avatar.primary-color(size='36px') 36
-			Avatar.blue.white-text 48
-			Avatar.red.white-text(size='62px') 62
+	h1 Hello {name}!
+	p With stores this time.
+	HelloWorldStoresDisplay
+	button(on:click!="{() => {$count++}}") Click to go from {$count} to {countPlusOne}
 </template>
 
 <script>
-	import { MaterialApp } from 'svelte-materialify';
 	import { count } from './store.js'
 	import HelloWorldStoresDisplay from './HelloWorldStoresDisplay.svelte'
-	import Avatar from 'svelte-materialify/src/components/Avatar';
+
 	export let name;
 
 	$: countPlusOne = $count + 1;
